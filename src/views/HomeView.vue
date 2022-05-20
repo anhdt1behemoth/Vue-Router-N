eeaf1b1438025ca0f967bd1599e2391fac643e6b
anhdt1behemoth <anhdt@behemoth.vn>
2022-05-20 11:19:19 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+   <button @click="$router.push('/user')">User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  methods: {
+    gotoUserPage() {
+      this.$router.push({
+        name: 'user',
+        path: '/user',
+        query: {id: 12341234}
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/UserView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>User {{$route.params.id}}</h1>
+    <button @click="back">back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  name: 'UserView',
+  methods: {
+    back() {
+     alert(this.id)
+    }
+  }
+}
+</script>
